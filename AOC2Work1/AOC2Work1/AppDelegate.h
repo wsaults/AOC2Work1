68b959d116f62931db99767e1bb7d5263710db94
William Saults <wsaults@email.itt-tech.edu>
2012-11-26 20:06:57 -0600

    Created the project.

---
+++ AOC2Work1/AOC2Work1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOC2Work1
+//
+//  Created by Will Saults on 11/26/12.
+//  Copyright (c) 2012 Fullsail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOC2Work1/AOC2Work1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AOC2Work1
+//
+//  Created by Will Saults on 11/26/12.
+//  Copyright (c) 2012 Fullsail. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
